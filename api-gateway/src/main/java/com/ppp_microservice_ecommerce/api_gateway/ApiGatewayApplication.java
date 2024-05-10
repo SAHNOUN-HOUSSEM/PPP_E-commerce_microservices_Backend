6cfbbd1aa317aa77d19b22089b63664dc9098a2e
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-05-10 12:28:44 +0100

    (gateway+auth-server): user authentication

--- api-gateway/src/main/java/com/ppp_microservice_ecommerce/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ppp_microservice_ecommerce/api_gateway/ApiGatewayApplication.java
@@ -3,10 +3,14 @@ package com.ppp_microservice_ecommerce.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableFeignClients(
+        basePackages = "com.ppp_microservice_ecommerce.clients"
+)
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
