0830f07fb118c6977ad898844626dd15036c8daf
FkihMehdi <123999605+FkihMehdi@users.noreply.github.com>
2024-06-02 13:20:45 +0200

    Product service (#27)

--- product-service/src/main/java/com/ppp_microservice_ecommerce/products/service/ProductService.java
+++ product-service/src/main/java/com/ppp_microservice_ecommerce/products/service/ProductService.java
@@ -2,7 +2,11 @@ package com.ppp_microservice_ecommerce.products.service;
 
 import com.ppp_microservice_ecommerce.clients.orders.OrderRequest;
 import com.ppp_microservice_ecommerce.products.dto.ProductResponse;
+import com.ppp_microservice_ecommerce.products.entities.Brand;
+import com.ppp_microservice_ecommerce.products.entities.Category;
 import com.ppp_microservice_ecommerce.products.entities.Product;
+import com.ppp_microservice_ecommerce.products.repository.BrandRepository;
+import com.ppp_microservice_ecommerce.products.repository.CategoryRepository;
 import com.ppp_microservice_ecommerce.products.repository.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,9 +23,13 @@ import java.util.stream.Collectors;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final BrandRepository brandRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, BrandRepository brandRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.brandRepository = brandRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public Page<Product> getProducts(
@@ -78,5 +86,24 @@ public class ProductService {
     public Map<Integer, String> getProductNames(Set<Integer> productIDs) {
         return productRepository.findAllById(productIDs).stream()
                 .collect(Collectors.toMap(Product::getId, Product::getName));
+    }
+
+    public void updateProduct(Integer id, Product product) {
+        Product productToUpdate = productRepository.findById(id).orElse(null);
+        Brand brand = brandRepository.findById(product.getBrand().getId()).orElse(null);
+        Category category = categoryRepository.findById(product.getCategory().getId()).orElse(null);
+        if (productToUpdate != null) {
+            productToUpdate.setName(product.getName());
+            productToUpdate.setPrice(product.getPrice());
+            productToUpdate.setQuantity(product.getQuantity());
+            productToUpdate.setDescription(product.getDescription());
+            productToUpdate.setBrand(brand);
+            productToUpdate.setCategory(category);
+            productRepository.save(productToUpdate);
+        }
+    }
+
+    public void deleteProduct(Integer id) {
+        productRepository.deleteById(id);
     }
 }
